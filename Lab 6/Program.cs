558a055b6605fb8ad85b4ec7d09656ead7a98c49
Lizzi <lizaveta9223@gmail.com>
2020-11-01 22:58:16 +0300

    1th & 2nd tasks

---
+++ Lab 6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Info ship = new Ship(false, 76);
+            Vehicle boat = new Boat(b: 5);
+            Vehicle steamer = new Steamer(2001);
+            Info corvette = new Corvette("Ususal");
+            VehicleDoes sailboat = new Sailboat("red", "silk");
+
+            // IS
+            if (sailboat is Vehicle) Console.WriteLine("ship можно привести к классу Vehicle.");
+            else Console.WriteLine("ship нельзя привести к классу Vehicle.");
+            if (ship is Corvette) Console.WriteLine("ship можно привести к классу Corvette.");
+            else Console.WriteLine("ship нельзя привести к классу Corvette.");
+            // AS
+            Object check = steamer as Vehicle;
+            if (check == null) Console.WriteLine("steamer нельзя привести к классу Vehicle");
+            else Console.WriteLine("steamer можно привести к классу Vehicle");
+            check = boat as Ship;
+            if (check == null) Console.WriteLine("boat нельзя привести к классу Ship\n");
+            else Console.WriteLine("boat можно привести к классу Ship\n");
+
+            // var list = (steamer, boat, ship, sailboat, corvette);
+            Printer printer = new Printer();
+            ArrayList list = new ArrayList();
+            list.Add(steamer);
+            list.Add(boat);
+            list.Add(ship);
+            list.Add(sailboat);
+            list.Add(corvette);
+            for (var i = 0; i < list.Count; i++)
+                printer.IAmPrinting(list[i] as Vehicle);
+
+            
+        }
+    }
+}
---
+++ Lab 6/Vehicle.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    enum Days
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    struct Users
+    {
+        public string name;
+        public byte age;
+        public void Show()
+        {
+            Console.WriteLine($"Имя пользователя - {name}, возраст - {age}.");
+        }
+    }
+    interface Info
+    {
+        bool WannaBuy();
+    }
+    interface VehicleDoes
+    {
+        void Sailing(bool a);
+    }
+    abstract class Vehicle : VehicleDoes
+    {
+        public abstract bool WannaBuy();
+        public string captain { get; set; }
+        public sbyte speed { get; set; }
+        public virtual void Display()
+        {
+            Console.WriteLine($"This vehicle captain's name is {captain}, speed is {speed} km/h.");
+        }
+        public bool sailing;
+        public virtual void Sailing(bool a)
+        {
+            if (a) Console.WriteLine("Сейчас судно находится в плавании.");
+            else Console.WriteLine("Сейчас судно НЕ находится в плавании.");
+        }
+    }
+    partial class Ship : Vehicle, Info
+    {
+        public bool battle_ship;
+        public int length;
+        public Ship(bool a = false, int b = 0)
+        {
+            battle_ship = a;
+            length = b;
+        }        
+    }
+    sealed class Steamer : Vehicle
+    {
+        public int year_of_creation;
+        public Steamer(int a)
+        {
+            year_of_creation = a;
+        }
+        class Steam_engine
+        {
+            public string type;
+        }
+        public override bool WannaBuy()
+        {
+            return false;
+        }
+        public override string ToString()
+        {
+            return ($"Тип объекта = {this.GetType()}; Имя капитана = {captain}; Скорость судна = {speed}; Судно сейчас находится в плавании? {sailing}; " +
+                        $"\nГод создания -  {year_of_creation}.");
+        }
+    }
+    class Boat : Vehicle
+    {
+        public bool engine;
+        public byte capacity;
+        public Boat(bool a = false, byte b = 0)
+        {
+            engine = a;
+            capacity = b;
+        }
+        public override string ToString()
+        {
+            return ($"Тип объекта = {this.GetType()}; Имя капитана = {captain}; Скорость судна = {speed}; Судно сейчас находится в плавании? {sailing}; " +
+                        $"Эта лодка имеет двигатель? {engine}. Вместимоть это лодки равна {capacity} людей.");
+        }
+        public override bool Equals(object obj)
+        {
+            if (this.GetType() != obj.GetType())
+                return false;
+            Boat boat = (Boat)obj;
+            if ((this.engine == boat.engine) && (this.capacity == boat.capacity))
+                return true;
+            else return false;
+        }
+        public override int GetHashCode()
+        {
+            return engine.GetHashCode();
+        }
+        public override bool WannaBuy()
+        {
+            return false;
+        }
+    }
+    class Sailboat : Ship
+    {
+        public string sail_color;
+        public string sail_material;
+        public Sailboat(string a = "", string b = "")
+        {
+            sail_color = a;
+            sail_material = b;
+        }
+        public override string ToString()
+        {
+            return ($"Тип объекта = {this.GetType()}; Имя капитана = {captain}; Скорость судна = {speed}; Судно сейчас находится в плавании? {sailing}; " +
+                         $"\nЭто боевое судно? {battle_ship}; Длина судна = {length}м. Цвет парусов - {sail_color}; Материал парусов - {sail_material}.");
+        }
+    }
+    class Corvette : Ship
+    {
+        public string type;
+        public Corvette(string s)
+        {
+            type = s;
+        }
+        public override string ToString()
+        {
+            return ($"Тип объекта = {this.GetType()}; Имя капитана = {captain}; Скорость судна = {speed}; Судно сейчас находится в плавании? {sailing}; " +
+                        $"\nЭто боевое судно? {battle_ship}; Длина судна = {length}м. Тип корвета - {type}.");
+        }
+    }
+    class Printer
+    {
+        public void IAmPrinting(Vehicle obj)
+        {
+            Console.WriteLine();
+            Console.WriteLine(obj.GetType());
+            Console.WriteLine(obj.ToString());
+            Console.WriteLine();
+        }
+    }
+}
